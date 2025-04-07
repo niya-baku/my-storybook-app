c2802a9600403d7458045cd1e910c49294e8ede3
baku_nishiya <planet.of.early.summer.rain@gmail.com>
2025-04-07 12:15:42 +0900

    Adding an Alert Component /chromatic

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export interface AlertProps {
-  type: 'info' | 'success' | 'warning' | 'error';
+  type: 'info' | 'success' | 'warning' | 'error' | 'neutral';
   title: string;
   message: string;
   onClose?: () => void;
@@ -18,6 +18,7 @@ const Alert: React.FC<AlertProps> = ({
     success: 'bg-green-50 border-green-400 text-green-700',
     warning: 'bg-yellow-50 border-yellow-400 text-yellow-700',
     error: 'bg-red-50 border-red-400 text-red-700',
+    neutral: 'bg-gray-50 border-gray-400 text-gray-700',
   };
   
   const iconMap = {
@@ -39,6 +40,11 @@ const Alert: React.FC<AlertProps> = ({
     error: (
       <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
         <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clipRule="evenodd"></path>
+      </svg>
+    ),
+    neutral: (
+      <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+        <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd"></path>
       </svg>
     ),
   };
